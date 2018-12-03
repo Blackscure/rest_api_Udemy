5cd47dd99a149e52971e52ca97e3e648a735d5b5
Blackscure <Wekesabuyahi@gmail.com>
2018-12-03 20:44:24 +0300

    app file

--- src/app.py
+++ src/app.py
@@ -9,14 +9,23 @@ app.secret_key = 'hezzy '
 
 @app.route('/')
 def hello_method():
+    return render_template('home.html')
+
+@app.route('/login')
+def login_template():
     return render_template("login.html")
+
+
+@app.route('/register')
+def register_template():
+    return render_template("register.html")
 
 @app.before_first_request
 def initialize_database():
     Database.initialize()
 
 
-@app.route('/login',methods=['POST'])
+@app.route('/auth/login',methods=['POST'])
 def login_user():
     email = request.form['email']
     password = request.form['password']
@@ -28,6 +37,20 @@ def login_user():
     #import pdb; pdb.set_trace();
 
     return render_template("profile.html", email=session['email'])
+
+
+
+@app.route('/auth/register', methods=['POST'])
+def register_user():
+     email = request.form['email']
+     password = request.form['password']
+
+     User.register(email, password)
+
+     return render_template("profile.html", email=session['email'])
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000 )
